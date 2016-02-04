6211be4dcd20832d055c88c850432926b8d40cca
brnikita <nikita-bragin@mail.ru>
2016-02-04 09:38:13 +0300

    Added policy for the company

--- api/policies/isCompanyOwner.js
+++ api/policies/isCompanyOwner.js
@@ -1,3 +1,35 @@
 /**
- * Created by user on 04.02.2016.
+ * Checking if user is owner of the company or not
  */
+module.exports = function (request, response, next) {
+  var params = request.param(),
+    companyId = params.id,
+    profile = request.user,
+    profileId = profile.id;
+
+  if (typeof companyId === 'undefined') {
+    return response.send(400, {
+      code: 'error.identificator.not.found',
+      message: 'You should specify company\'s id'
+    });
+  }
+
+  Company.findOne({id: companyId}).exec(function (error, company) {
+    if (error) {
+      return response.send(500, {
+        code: 'error',
+        message: error
+      });
+    }
+
+    if (profileId !== company.owner) {
+      return response.send(403, {
+        code: 'no.access',
+        message: 'You don\'t have access to the resource'
+      });
+    }
+
+    request.company = company;
+    next();
+  });
+};
