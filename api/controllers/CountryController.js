18dad7bd5f4a0ac0572074a8452b535ca35a7ea4
brnikita <nikita-bragin@mail.ru>
2016-02-15 16:32:23 +0300

    Initialized country, currency, language controllers

---
+++ api/controllers/CountryController.js
@@ -0,0 +1,11 @@
+/**
+ * CountriesController
+ *
+ * @description :: Server-side logic for managing countries
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/CurrencyController.js
@@ -0,0 +1,11 @@
+/**
+ * CurrenciesController
+ *
+ * @description :: Server-side logic for managing currencies
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
