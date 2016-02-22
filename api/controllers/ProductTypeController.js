a74e062b1f694b2b460abcee8fc2bdb7177bf1d4
Nikita <nikita-bragin@mail.ru>
2016-02-23 01:48:11 +0300

    Updated permissions for creating productType

--- api/controllers/ProductTypeController.js
+++ api/controllers/ProductTypeController.js
@@ -9,24 +9,42 @@ var _ = require('underscore');
 
 module.exports = {
 
-  //TODO: need to check that Project exists
-  //TODO: need to check permissions to Project
   create: function (request, response) {
-    var productTypeData = request.body,
-      profile = request.user;
+    var user = request.user,
+      userId = user.id,
+      productTypeData = request.body,
+      projectId = productTypeData.project;
 
-    productTypeData.owner = profile.id;
-    ProductType.create(productTypeData).exec(function (error, productType) {
+    PermissionsService.getPermissionsByProject(userId, projectId, function (error, permission) {
+      var isOwner,
+        managerOfProducts;
+
       if (error) {
-        return response.send(500, {
-          code: 'error',
-          message: error
+        return response.serverError(error);
+      }
+
+      isOwner = permission.isOwner;
+      managerOfProducts = permission.productsPermission === 'manage';
+      if (!isOwner && !managerOfProducts) {
+        return response.send(403, {
+          code: 'access.denied',
+          message: 'Access denied'
         });
       }
 
-      response.send(200, {
-        code: 'successful',
-        productType: productType
+      productTypeData.owner = user.id;
+      ProductType.create(productTypeData).exec(function (error, productType) {
+        if (error) {
+          return response.send(500, {
+            code: 'error',
+            message: error
+          });
+        }
+
+        response.send(200, {
+          code: 'successful',
+          productType: productType
+        });
       });
     });
   },
