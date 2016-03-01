4e2e91b6e75e2e74e00277a2b2d9ae99c447c9eb
Nikita <nikita-bragin@mail.ru>
2016-03-01 17:03:09 +0300

    Added models Client and Customer

---
+++ api/models/Client.js
@@ -0,0 +1,61 @@
+/**
+ * Client.js
+ */
+
+var bcrypt = require('bcrypt-nodejs');
+module.exports = {
+
+  attributes: {
+    clientId: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+
+    clientSecret: {
+      type: 'string',
+      required: true
+    },
+
+    trustLevel: {
+      type: 'string'
+    },
+
+    compareSecret: function (clientSecret) {
+      return bcrypt.compareSync(clientSecret, this.clientSecret);
+    },
+
+    toJSON: function () {
+      var obj = this.toObject();
+      delete obj.clientSecret;
+
+      return obj;
+    }
+  },
+
+  afterCreate: function (client, next) {
+    Token.generateToken({
+      clientId: client.clientId
+    }).then(function () {
+      next();
+    });
+  },
+
+  beforeCreate: function (client, next) {
+    if (client.hasOwnProperty('clientSecret')) {
+      client.clientSecret = bcrypt.hashSync(client.clientSecret, bcrypt.genSaltSync(10));
+      return next(false, client);
+    }
+
+    next(null, client);
+  },
+
+  beforeUpdate: function (client, next) {
+    if (client.hasOwnProperty('clientSecret')) {
+      client.clientSecret = bcrypt.hashSync(client.clientSecret, bcrypt.genSaltSync(10));
+      return next(false, client);
+    }
+
+    next(null, client);
+  }
+};
--- api/models/Customer.js
+++ api/models/Customer.js
@@ -1,10 +1,10 @@
 /**
-* Customer.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
-
+ * Customer.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+var bcrypt = require('bcrypt-nodejs');
 module.exports = {
 
   attributes: {
@@ -12,10 +12,57 @@ module.exports = {
       type: 'string',
       required: true
     },
-    project: {
-      model: 'project',
+
+    email: {
+      type: 'email',
+      unique: true,
       required: true
+    },
+
+    password: {
+      type: 'string',
+      required: true,
+      columnName: 'encrypted_password',
+      minLength: 8
+    },
+
+    location: {
+      type: 'string'
+    },
+
+    verifiedAt: {
+      type : 'date'
+    },
+
+    comparePassword: function(password) {
+      return bcrypt.compareSync(password, this.password);
+    },
+
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+
+      return obj;
+    }
+  },
+
+  beforeCreate: function(user, next) {
+    if (user.hasOwnProperty('password')) {
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
+      next(false, user);
+
+    } else {
+      next(null, user);
+    }
+  },
+
+
+  beforeUpdate: function(user, next) {
+    if (user.hasOwnProperty('password')) {
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
+      next(false, user);
+    } else {
+      next(null, user);
     }
   }
 };
-
